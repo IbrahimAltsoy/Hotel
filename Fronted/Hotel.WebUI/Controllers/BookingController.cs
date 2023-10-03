1dd1b0d3afb9715fd7b8389f5dd6a2d9e5329ac4
IbrahimAltsoy <78874266+IbrahimAltsoy@users.noreply.github.com>
2023-10-04 00:57:34 +0300

    REzervsyon için controller eklendi entitit gözden geçirilmesi lazım

--- Fronted/Hotel.WebUI/Controllers/BookingController.cs
+++ Fronted/Hotel.WebUI/Controllers/BookingController.cs
@@ -1,12 +1,69 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Hotel.WebUI.Dtos.BookingDto;
+using Microsoft.AspNetCore.Mvc;
+using NToastNotify;
+using static Hotel.WebUI.ToastrMessage.ToastrMessage;
 
 namespace Hotel.WebUI.Controllers
 {
     public class BookingController : Controller
     {
+        private readonly HttpClient _httpClient;
+        private readonly IHttpClientFactory _clientFactory;
+        private readonly string _apiAdres = "https://localhost:7064/api/Booking";
+        private readonly IToastNotification _toastNotification;
+
+        public BookingController(IToastNotification toastNotification, HttpClient httpClient, IHttpClientFactory clientFactory)
+        {
+            _toastNotification = toastNotification;
+            _httpClient = httpClient;
+            _clientFactory = clientFactory;
+        }
         public IActionResult Index()
         {
             return View();
         }
+        [HttpGet]
+        public PartialViewResult AddBooking()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddBooking(CreateBookingDto createBooking)
+        {
+             //< input type = "hidden" class="form-control" asp-for="Status" placeholder="Status" value="Merhaba">
+             //           <input type = "hidden" class="form-control" asp-for="IsActive" value="true" placeholder="Email">
+             //           <input type = "hidden" class="form-control" asp-for="Description" placeholder="description" value="Merhaba Merhaba"> 
+             // bunları html tarafında yazıldı entiti özellikleri un required yapmayı unutma yapınca arka tarafta sil
+            if (ModelState.IsValid)
+            {
+                try
+                {
+
+                    var response = await _httpClient.PostAsJsonAsync(_apiAdres, createBooking);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        _toastNotification.AddSuccessToastMessage(MessajeToastr.ToastrAddSuccesfull(createBooking.Name),
+                        new ToastrOptions
+                        {
+                            Title = "Başarılı"
+                        });
+                        return RedirectToAction(nameof(Index), "Default");
+                    }
+
+                }
+                catch
+                {
+                    _toastNotification.AddSuccessToastMessage(MessajeToastr.ToastrAddUnSuccessfull(createBooking.Name),
+                        new ToastrOptions
+                        {
+                            Title = "Başarısız"
+                        });
+                    ModelState.AddModelError("", "Hata Oluştu!");
+                }
+            }
+
+            return View(createBooking);
+        }
+
     }
 }
---
+++ Fronted/Hotel.WebUI/ViewComponents/Booking/_BookingCoverImage.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel.WebUI.ViewComponents.Booking
+{
+    public class _BookingCoverImage:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
